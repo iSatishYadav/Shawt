965945fcdd74b46b17104d2daaec0a36d77a3b5d
Satish <monstersatish@live.com>
2025-02-23 22:00:34 +0530

    Fix type assertion in getBaseUrl function
    Updated the getBaseUrl function in main.ts to use a type assertion when accessing the href property of the element with ID 'api'. This change ensures compatibility with TypeScript's type checking.

--- Shawt/ClientApp/src/main.ts
+++ Shawt/ClientApp/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 export function getBaseUrl() {
-  return document.getElementById('api').href;
+  return (document.getElementById('api') as any).href;
 }
 
 const providers = [
